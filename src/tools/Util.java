c265790eb76ee64d0fabe175411a47ec7b5ccca7
clara0229 <clara.cunha@estudante.ifms.edu.br>
2025-09-20 14:37:36 -0400

    Fiz as pastas tools, img e view

---
+++ src/tools/Util.java
@@ -0,0 +1,15 @@
+package tools;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Clara Delgado
+ */
+public class Util {
+    
+}
